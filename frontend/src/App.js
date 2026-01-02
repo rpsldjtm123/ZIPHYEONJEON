9531e2baf7e2420336132965acde9a108391406b
Jihoon <wjdwlgns11q@gmail.com>
2026-01-02 16:25:20 +0900

    react 추가

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          실행 확인
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
